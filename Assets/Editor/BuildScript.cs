1a80ceecfaf3599e7a0dc9e7adcc60c8614d3de0
MikeySchool <mikeykrentz@u.boisestate.edu>
2020-06-18 14:41:39 -0600

    Changed build script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -10,7 +10,7 @@ namespace Assets.Editor
         static void Build()
         {
             string[] scenes = { "Assets/Scenes/SampleScene.unity" };
-            BuildPipeline.BuildPlayer(scenes, "D:/CDBProductivitySuite/Build/Inception/Inception/Build/Inception.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+            BuildPipeline.BuildPlayer(scenes, "D:/CDBProductivitySuite/Build/Inception/Build/Inception.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
         }
     }
 }
